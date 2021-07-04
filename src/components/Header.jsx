fdf16c9938955d6113c06865a444a2732aafc388
Sovazavr <vadimsopov00@mail.ru>
2021-07-04 12:06:35 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () =>
+{
+    return (
+        <header className='Head'>
+        <img src = 'https://png.pngtree.com/element_our/md/20180506/md_5aeedf0005b7a.png' />
+        
+      </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+
+
+const Profile = () =>
+{
+    return (
+      <div className='Content'>
+
+      <img className='backImg' src='https://www.pixelstalk.net/wp-content/uploads/2016/07/1080p-HD-Image-Nature-Desktop.jpg'/>
+      <div>
+      ava + description
+    </div>
+    <div>
+      My posts
+      <div>
+        new post
+      </div>
+      <div>
+        post 1
+      </div>
+      <div>
+        post 2
+      </div>
+      </div>
+    </div>
+    )
+}
+
+export default Profile;
