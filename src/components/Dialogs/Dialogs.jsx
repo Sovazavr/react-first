d15c1b40aa362b37416de0ac1c6c8f6c2a988868
Sovazavr <vadimsopov00@mail.ru>
2021-07-07 11:22:52 +0700

    messages verst

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,19 @@ import rename from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={rename.dialogs}>
+
+            <div className={rename.dialogs_item}>
+                <div className={rename.dialog}>Dmitry</div>
+                <div className={rename.dialog}>Andrey</div>
+                <div className={rename.dialog}>Sveta</div>
+            </div>
+
+            <div className={rename.messages}>
+                <div className={rename.message}>Hi</div>
+                <div className={rename.message}>How are you</div>
+                <div className={rename.message}>WAZZZAAARRR</div>
+            </div>
         </div>
     )
 }
