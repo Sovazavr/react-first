54848aca0182ecf66b8596994a8befd7683672e6
Sovazavr <vadimsopov00@mail.ru>
2021-07-18 15:26:48 +0700

    хз

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
+import Profile from './components/Profile/Profile'
 
-import { BrowserRouter, Route } from 'react-router-dom';
-import Music from './components/Music/Music';
-import Settings from './components/Settings/Settings';
-import News from './components/News/News';
-import {ThemeProvider, createTheme} from '@material-ui/core/styles'
-import {Paper} from '@material-ui/core'
-import React, {useState} from 'react'
-import DialogsContainer from './components/Dialogs/DialogsContainer';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Music from './components/Music/Music'
+import Settings from './components/Settings/Settings'
+import News from './components/News/News'
+import { ThemeProvider, createTheme } from '@material-ui/core/styles'
+import { Paper } from '@material-ui/core'
+import React, { useState } from 'react'
+import DialogsContainer from './components/Dialogs/DialogsContainer'
 
 const App = (props) => {
-
   
+
   return (
-   
-      <div className='app-wrapper'>
-        <Header/>
+  
+    <div className='app-wrapper'>
+      <Header />
 
-        <Navbar />
+      <Navbar />
 
 
-        <div className='app-wrapper-content'>
-          <Route path='/dialogs' render={() =>
-            <DialogsContainer
-              // messages={props.state.messagesPage.messages}
-              // dialogs={props.state.messagesPage.dialogs} 
-              store={props.store}/>} />
-          <Route path='/profile' render={() =>
-            <Profile store={props.store} />} />
-          <Route path='/news' render={() => <News />} />
-          <Route path='/music' render={() => <Music />} />
-          <Route path='/settings' render={() => <Settings />} />
-        </div>
+      <div className='app-wrapper-content'>
+        <Route path='/dialogs' render={() =>
+          <DialogsContainer
+            // messages={props.state.messagesPage.messages}
+            // dialogs={props.state.messagesPage.dialogs} 
+            store={props.store} />} />
+        <Route path='/profile' render={() =>
+          <Profile store={props.store} />} />
+        <Route path='/news' render={() => <News />} />
+        <Route path='/music' render={() => <Music />} />
+        <Route path='/settings' render={() => <Settings />} />
       </div>
-      
+    </div>
+    
+   
   );
 }
 
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,9 +1,14 @@
 import rename from './ProfileInfo.module.css'
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const ProfileInfo = () => {
     return (
         <div>
-            <img className={rename.backImg} src='https://www.peku-poccuu.ru/images/city/156.jpg' />
+            <Card>
+                
+                 <img className={rename.backImg} src='https://www.peku-poccuu.ru/images/city/156.jpg' /> 
+            </Card>
             <div className={rename.descriptionBlock}>
                 ava + description
             </div>
