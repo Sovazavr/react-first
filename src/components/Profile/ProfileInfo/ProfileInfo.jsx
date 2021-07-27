3e2a6ba13195d05d57c6f0feadf16abf15a47e43
Sovazavr <vadimsopov00@mail.ru>
2021-07-27 13:33:49 +0700

    status and local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import rename from './ProfileInfo.module.css'
 import Card from '@material-ui/core/Card';
 import Preloader from '../../common/preloader/preloader.js'
+import ProfileStatus from './ProfileStatus'
 
 const ProfileInfo = (props) => {
     if(!props.profile) {
@@ -8,12 +9,13 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-            <Card>
+            {/* <Card>
                 
                  <img className={rename.backImg} src='https://www.peku-poccuu.ru/images/city/156.jpg' /> 
-            </Card>
+            </Card> */}
             <div className={rename.descriptionBlock}>
                 <img src={props.profile.photos.large} />
+                <ProfileStatus status='hello me'/>
                 <div>
                     {props.profile.aboutMe}
                 </div>
