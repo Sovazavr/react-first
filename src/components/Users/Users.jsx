8a0dfcb9e086aaa5f73bcdf06f8771a6d837a71a
Sovazavr <vadimsopov00@mail.ru>
2021-08-17 16:41:49 +0700

    paginator

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -9,22 +9,18 @@ import User from './User/User';
 
 let Users = (props) => {
     
-    // let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
-
-    // let pages = []
-    // for (let i = 1; i <= pagesCount; i++) {
-    //     pages.push(i);
-    // }
+   
     
     return (
         <div>
-           <Paginator totalUsersCount={props.totalUsersCount} pageSize={props.pageSize} 
+           <Paginator totalItemsCount={props.totalUsersCount} pageSize={props.pageSize} 
             currentPage={props.currentPage} onPageChanged={props.onPageChanged}/>
             {
 
                 props.users.map(u => <User key={u.id} user={u} followingInProgress={props.followingInProgress} 
                     unfollow={props.unfollow} follow={props.follow} />)
             }
+            
         </div>
     )
 }
